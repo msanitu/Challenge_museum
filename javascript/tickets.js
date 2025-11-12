6e6fe6df258a98c77bde3c3b28a7841253525096
MikelFuensanta <m_fuensantamen@fpzornotzalh.eus>
2025-11-12 14:57:10 +0100

    tickets almost done

--- javascript/tickets.js
+++ javascript/tickets.js
@@ -5,15 +5,15 @@ function Calculate() {
 
     const ticketNum = parseInt(document.getElementById("quantity").value);
 
-    if (isNaN(ticketNum) || ticketNum < 1) {
+    if (ticketNum < 1) {
         alert("Please enter a valid ticket quantity (min 1)");
-
     } else {
         const ticketType = parseFloat(document.querySelector('input[name="type"]:checked').value);
         let total = ticketNum * ticketType;
 
         const guideCheckbox = document.getElementById("guide");
         if (guideCheckbox && guideCheckbox.checked) {
+           document.getElementById("guidance").style.visibility = "visible";
             total += parseFloat(guideCheckbox.value);
         }
 
@@ -21,39 +21,34 @@ function Calculate() {
 
         if (discountInput == "MUSEUM10") {
             total = +(total * 0.9);
+          document.getElementById("code").style.visibility = "visible";
         }
 
-        document.getElementById("result").textContent = 
-        '<p id="result">'
-                + '"Amount to pay: "' + total.toFixed(2) + '" €"' +
-            '</p>'+
-            '<p>'
-                + 'Name:'+
-            '</p>'
-            '<p>'
-                'E-mail:'
-            '</p>'
-            '<p>'
-                'Date:'
-            '</p>'
-            '<p>'
-                'Time:'
-            '</p>'
-            '<p>'
-                'Ticket amount:'
-            '</p>'
-            '<p>'
-                'Ticket type:'
-            '</p>'
-            '<div class="opcionales" id="opcionales">'
-                '<p>'
-                    'Guidenace:'
-                '</p>'
-                '<p>'
-                    'Discount:'
-                '</p>;';
+        if (ticketType == 12) {
+            const type = "Adult";
+        } else if (ticketType == 8) {
+            const type = "Student";
+        } else {
+            const type = "Child";
+        }
+
+        document.getElementById("moneyAmount").textContent = "Amount to pay: " + total.toFixed(2) + " €";
+        document.getElementById("name").textContent = "Name: " + (document.getElementById("nameForm").value);
+        document.getElementById("email").textContent = "E-mail: " + (document.getElementById("emailForm").value);
+        document.getElementById("date").textContent = "Date: " + (document.getElementById("dateday").value) + "/" + (document.getElementById("datemonth").value) + "/" + (document.getElementById("dateyear").value);
+        document.getElementById("time").textContent = "Time: " + (document.getElementById("hour").value);
+        document.getElementById("ticketAmount").textContent = "Ticket Amount: " + (document.getElementById("quantity").value);
+        document.getElementById("typeTicket").textContent = "Ticket type: " + type;
+        document.getElementById("guidance").textContent = "Guidenace: Yes";
+        document.getElementById("code").textContent = "Discount: 10%";
     }
 
 
+
+
+
 }
+
+
+
 
