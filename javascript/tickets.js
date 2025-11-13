3f77ac2760255f11942b8160980bdc854861a69b
MikelFuensanta <m_fuensantamen@fpzornotzalh.eus>
2025-11-13 09:16:28 +0100

    cambio en tickets

--- javascript/tickets.js
+++ javascript/tickets.js
@@ -8,12 +8,14 @@ function Calculate() {
     if (ticketNum < 1) {
         alert("Please enter a valid ticket quantity (min 1)");
     } else {
+        document.getElementById("result").style.display = "block";
         const ticketType = parseFloat(document.querySelector('input[name="type"]:checked').value);
         let total = ticketNum * ticketType;
 
         const guideCheckbox = document.getElementById("guide");
         if (guideCheckbox && guideCheckbox.checked) {
-           document.getElementById("guidance").style.visibility = "visible";
+            document.getElementById("guidance").style.visibility = "visible";
+            document.getElementById("guidance").textContent = "Guidenace: Yes";
             total += parseFloat(guideCheckbox.value);
         }
 
@@ -21,16 +23,10 @@ function Calculate() {
 
         if (discountInput == "MUSEUM10") {
             total = +(total * 0.9);
-          document.getElementById("code").style.visibility = "visible";
+            document.getElementById("code").style.visibility = "visible";
+            document.getElementById("code").textContent = "Discount: 10%";
         }
-
-        if (ticketType == 12) {
-            const type = "Adult";
-        } else if (ticketType == 8) {
-            const type = "Student";
-        } else {
-            const type = "Child";
-        }
+  
 
         document.getElementById("moneyAmount").textContent = "Amount to pay: " + total.toFixed(2) + " €";
         document.getElementById("name").textContent = "Name: " + (document.getElementById("nameForm").value);
@@ -38,9 +34,9 @@ function Calculate() {
         document.getElementById("date").textContent = "Date: " + (document.getElementById("dateday").value) + "/" + (document.getElementById("datemonth").value) + "/" + (document.getElementById("dateyear").value);
         document.getElementById("time").textContent = "Time: " + (document.getElementById("hour").value);
         document.getElementById("ticketAmount").textContent = "Ticket Amount: " + (document.getElementById("quantity").value);
-        document.getElementById("typeTicket").textContent = "Ticket type: " + type;
-        document.getElementById("guidance").textContent = "Guidenace: Yes";
-        document.getElementById("code").textContent = "Discount: 10%";
+
+
+
     }
 
 
